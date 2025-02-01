7402d8ba765a712810bcb61b39e5c10ccf771869
Elian <elian.dap@outlook.com>
2025-02-01 16:04:43 -0500

    add sprites and ui scene

--- UnityProject/Assets/Scripts/Movement.cs
+++ UnityProject/Assets/Scripts/Movement.cs
@@ -13,6 +13,5 @@ public class Movement : MonoBehaviour
     private void Update()
     {
        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") *speed, Input.GetAxis("Vertical")*speed);
-
     }
 }
