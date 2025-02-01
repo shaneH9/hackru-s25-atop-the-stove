4cede631d8ff2018738fecc0c681849639200cda
Elian <elian.dap@outlook.com>
2025-02-01 15:09:40 -0500

    use arrow funcs

--- UnityProject/Assets/Scripts/UI/UIScreen.cs
+++ UnityProject/Assets/Scripts/UI/UIScreen.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class UIScreen : MonoBehaviour
 {
-    public virtual void Show()
-    {
-        gameObject.SetActive(true);
-    }
-
-    public virtual void Hide()
-    {
-        gameObject.SetActive(false);
-    }
+    public virtual void Show() => gameObject.SetActive(true);
+    public virtual void Hide() => gameObject.SetActive(false);
 }
